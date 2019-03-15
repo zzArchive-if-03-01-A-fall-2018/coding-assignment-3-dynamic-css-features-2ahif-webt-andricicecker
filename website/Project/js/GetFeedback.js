cdb254218168025420f47d5830e6e2c1d8539107
Andricic Daniel <dani.andricic@gmail.com>
2019-03-15 08:49:12 +0100

    Updad

--- website/Project/js/GetFeedback.js
+++ website/Project/js/GetFeedback.js
@@ -1,10 +1,15 @@
 function GetTextFields()
 {
-
-  SendToServer(firstname, lastname, comment);
+  let username;
+  if(!document.getElementById("blankCheckbox").checked)
+  {
+    username = document.getElementById("Username");
+  }
+  let comment = document.getElementById("Comment");
+  SendToServer(username, comment);
 }
 
 function SendToServer(firstname, lastname, comment)
 {
-
+  
 }
