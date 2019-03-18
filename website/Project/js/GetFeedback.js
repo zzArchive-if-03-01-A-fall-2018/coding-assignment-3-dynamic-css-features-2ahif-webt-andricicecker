ee5310ee49e9f24363c7cd58f2659a8538a1c8de
Andricic Daniel <dani.andricic@gmail.com>
2019-03-18 08:41:14 +0100

    Updated Javacript-Files

--- website/Project/js/GetFeedback.js
+++ website/Project/js/GetFeedback.js
@@ -4,12 +4,26 @@ function GetTextFields()
   if(!document.getElementById("blankCheckbox").checked)
   {
     username = document.getElementById("Username");
+    username.disabled = true;
   }
   let comment = document.getElementById("Comment");
   SendToServer(username, comment);
 }
 
-function SendToServer(firstname, lastname, comment)
+function SendToServer(username, comment)
 {
-  
+  var mySQL = require('mysql');
+
+  var con = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword"
+  });
+
+  con.connect(function(err)
+  {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 }
