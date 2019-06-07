f3ba6209d05e100565b8bc933a1c098082474e28
Andricic Daniel <if160082@EDU.HTL-LEONDING.AC.AT>
2019-06-07 14:24:44 +0200

    Cleaned up a bit of the mess.

--- Project/js/fetch.js
+++ Project/js/fetch.js
@@ -1,6 +1,6 @@
-function fetchAPI()
+/*function fetchAPI()
 {
-  fetch('http://localhost:3000/posts/4')
+  fetch('http://212.241.114.236:3000/posts/4')
   .then(function(response) {
     return response.json();
   })
@@ -22,4 +22,51 @@ function fetchAPI()
     console.log(JSON.stringify(myJson.content));
     console.log(JSON.stringify(myJson.id));
   });
+}
+*/
+function FetchComments(username, comment)
+{
+  fetchAPI();
+}
+
+function fetchAPI()
+{
+  fetch("http://212.241.114.236:3000/db.json").onload = function()
+  {
+    alert('Der Server ist online!');
+  };
+  .then(function (response) {
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.url);
+    console.log(response.type);
+
+    return response.json();
+  })
+  .then(
+    function (json) {
+      console.log(json["Test"]);
+    }
+  );
+  fetch("http://212.241.114.236:3000/db.json").onerror = function()
+  {
+    alert('Der Server ist nicht online!');
+  };
+}
+
+function PostComment()
+{
+  (async () => {
+  const rawResponse = await fetch('https://212.241.114.236:3000/post', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({a: 1, b: 'Textual content'})
+  });
+  const content = await rawResponse.json();
+
+  console.log(content);
+})();
 }
